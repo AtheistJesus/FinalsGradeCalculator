4c7ce1b89d9871d6d95356e529b5adcba866c449
AtheistJesus <atheistjesus@users.noreply.github.com>
2015-07-04 14:27:41 -0700

    This updates manifest and build.gradle for 1.1 release to google play store.

--- app/src/main/java/com/avalani/kyle/finalsgradecalculator/MainActivity.java
+++ app/src/main/java/com/avalani/kyle/finalsgradecalculator/MainActivity.java
@@ -24,7 +24,7 @@ private static final int RESULT_SETTINGS = 1;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //testing2
+        //testing3
 
         ImageButton submitbutton = (ImageButton) findViewById(R.id.submitbutton);
         submitbutton.setOnClickListener(new View.OnClickListener() {
